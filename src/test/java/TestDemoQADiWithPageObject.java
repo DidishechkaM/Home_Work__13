bf120bbb6ec424b2ecee4fa2017b6ac082cef81a
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-03-29 10:44:59 +0300

    main test

---
+++ src/test/java/TestDemoQADiWithPageObject.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+public class TestDemoQADiWithPageObject extends TestBase {
+
+    String userName = "Dian",
+            lastNme = " Maksimowa",
+            userEmail = "maksimowa@yandex.ru";
+    @Test
+    void fillTest() {
+
+        registrationPage.openPage()
+                .executeJS()
+                .setFirsName(userName)
+                .setLastName(lastNme)
+                .setEmail(userEmail)
+                .setGender("Female")
+                .setNumber("8996266848")
+                .setBirthDay("13", "March", "1993")
+                .setSubject("Maths")
+                .setCheckbox("Sports")
+                .uploadFile("src/test/resources/pictures/temp.png")
+                .setAddress("Address")
+                .setState("Uttar Pradesh")
+                .setstateCity("Agra")
+                .submitclick();
+        registrationPage.verifiability();
+        registrationPage.verifyValueTable("Student Name", userName + lastNme);
+        registrationPage.verifyValueTable("Student Email", userEmail);
+        registrationPage.verifyValueTable("Gender", "Female");
+        registrationPage.verifyValueTable("Mobile", "8996266848");
+        registrationPage.verifyValueTable("Date of Birth", "13 March,1993");
+        registrationPage.verifyValueTable("Subjects", "Maths");
+        registrationPage.verifyValueTable("Hobbies", "Sports");
+        registrationPage.verifyValueTable("Picture", "temp.png");
+        registrationPage.verifyValueTable("Address", "Address");
+        registrationPage.verifyValueTable("State and City", "Uttar Pradesh Agra");
+        registrationPage.closeModal();
+    }
+
+}
