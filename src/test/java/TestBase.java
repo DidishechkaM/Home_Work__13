106cfc5af31690a2b2de920a2a253c340b1294c8
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-04-01 13:47:05 +0300

    add attachlogs

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -32,8 +34,14 @@ public class TestBase {
              @BeforeEach
                      void addListener() {
                  SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
             }
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screen");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
         }
 
 
